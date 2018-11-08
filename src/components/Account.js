a192c0bb6dce90c5f6c162d9c8f1e70c95f1b245
ghildreth <gabrielantonhildreth@gmail.com>
2018-11-07 22:37:20 -0800

    deleted random file and updated code to display current user

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import AuthUserContext from './AuthUserContext';
+import { PasswordForgetForm } from './PasswordForget';
+import PasswordChangeForm from './PasswordChange';
 
-const Account = () =>
-    <div>
-        <h1>Account Page </h1>
-    </div>
+const AccountPage = () =>
+    <AuthUserContext.Consumer>
+        {authUser =>
+            <div>
+{ console.log(authUser) }
+                <h1>Account details:</h1>
+                <h1>Email: {authUser.email}</h1>
+                <PasswordForgetForm />
+                <PasswordChangeForm />
+            </div>
+        }
+    </AuthUserContext.Consumer>
 
-export default Account;
+export default AccountPage;
